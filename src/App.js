1be9f6068a831eb56395bd0bbbda8a97a4ad48cd
UBnkantah <ubongnkantahuwem@gmail.com>
2023-10-19 15:16:27 +0000

    light-dark-mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FaSun, FaMoon } from "react-icons/fa";
+import { useTheme } from "next-themes";
 
 function App() {
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  const themeBox = () => {
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "dark") {
+      return <FaSun />;
+    } else {
+      return <FaMoon />;
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +24,18 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="dark:text-red-500 text-green-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        {themeBox()}
+      </button>
       </header>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'next-themes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider attribute='class' enableSystem={true}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
